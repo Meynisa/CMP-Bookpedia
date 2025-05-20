ad44c3c82e8a762457e825fd00a6e6164a35fe1b
Aprikot <mandani850@gmail.com>
2025-05-20 11:23:11 +0700

    - fetch book details

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
@@ -1,5 +1,6 @@
 package com.plcoding.bookpedia.book.data.network
 
+import com.plcoding.bookpedia.book.data.dto.BookWorkDto
 import com.plcoding.bookpedia.book.data.dto.SearchResponseDto
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.core.data.safeCallApi
@@ -19,7 +20,7 @@ class KtorRemoteBookDataSource(
         query: String,
         resultLimit: Int?
     ): Result<SearchResponseDto, DataError.Remote>{
-        return safeCallApi {
+        return safeCallApi <SearchResponseDto> {
             httpClient.get(urlString = "$BASE_URL/search.json"
             ){
                 parameter("q", query)
@@ -27,6 +28,14 @@ class KtorRemoteBookDataSource(
                 parameter("language", "eng")
                 parameter("fields", "key,title,author_name,author_key,cover_edition_key,cover_i,ratings_average,ratings_count,first_publish_year,language,number_of_pages_median,edition_count")
             }
+        }
+    }
+
+    override suspend fun getBookDetails(bookWorkId: String): Result<BookWorkDto, DataError.Remote> {
+        return safeCallApi <BookWorkDto>{
+            httpClient.get(
+                urlString = "$BASE_URL/works/$bookWorkId.json"
+            )
         }
     }
 }
